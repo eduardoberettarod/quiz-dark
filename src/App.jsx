b857bfcf59181309446dea98d93b29e11efc647e
eduardoberettarod <eduardo.berettarod@gmail.com>
2026-01-02 14:59:17 -0300

    Iniciando o projeto
    iniciei o projeto e limpei as pastas

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <img src="/public/image/hud.png" alt="" className='teste' />
+    </>
+  )
+}
+
+export default App
