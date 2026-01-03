af79a2ceb3be532864bec2457d4975bfec87f977
eduardoberettarod <eduardo.berettarod@gmail.com>
2026-01-03 00:11:49 -0300

    Hud
    Completei o hud do quiz

--- src/components/Hud/Hud.jsx
+++ src/components/Hud/Hud.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import './Hud.css'
 
 const Hud = () => {
     return (
         <>
-            <div className='d-flex align-items-center'>
-                <div>
-                <img src="/public/image/skull-circle.png" alt="" className='skull-icon-hud image' />
+            <div className="d-flex align-items-center justify-content-between mt-3 px-3">
+
+                <img
+                    src="/image/skull-circle.png"
+                    alt=""
+                    className="skull-icon-hud"
+                />
+
+                <div className="d-flex gap-1">
+                    <img src="/image/blood-health.png" className="health" />
+                    <img src="/image/blood-health.png" className="health" />
+                    <img src="/image/blood-health.png" className="health" />
                 </div>
 
-                <div>
-                    <img src="/public/image/blood.png" alt="" className='b' />
-                    <img src="/public/image/blood.png" alt="" className='b' />
-                    <img src="/public/image/blood.png" alt="" className='b' />
-                </div>
             </div>
+
         </>
     )
 }
